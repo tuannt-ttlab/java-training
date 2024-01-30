5983907ecdf4b24984ee6757d6f74442767d7c20
tuannt <tuannt@tokyotechlab.com>
2024-01-30 15:00:02 +0700

    Add feature

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 	public static void main(String[] args) {
+		// TODO: Add code here
 		SpringApplication.run(MainApplication.class, args);
 	}
+
+
 }
